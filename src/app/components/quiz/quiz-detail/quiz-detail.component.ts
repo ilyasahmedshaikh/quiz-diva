fe2928a17703bbd2f23432d439c885fbd5fa44f8
ilyas ahmed <ilyasahmed081@gmail.com>
2021-08-10 00:56:32 +0500

    quiz detail screen done and other fixes done

--- src/app/components/quiz/quiz-detail/quiz-detail.component.ts
+++ src/app/components/quiz/quiz-detail/quiz-detail.component.ts
@@ -7,9 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuizDetailComponent implements OnInit {
 
+  newList: any = [];
+
   constructor() { }
 
   ngOnInit(): void {
+    this.newList = [
+      { image: "assets/img/nq1.jpg", name: "ULTIMATE CHRISTMAS QUIZ" },
+      { image: "assets/img/nq2.jpg", name: "SPOT SANTA QUIZ" },
+      { image: "assets/img/nq3.jpg", name: "ULTIMATE HALLOWEEN QUIZ" },
+      { image: "assets/img/nq4.jpg", name: "AMONG US TASK QUIZ" },
+      { image: "assets/img/nq5.jpg", name: "FALL GUYS MISSING PIECE QUIZ" }
+    ]
   }
 
 }
